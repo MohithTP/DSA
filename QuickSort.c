97b55323ab7359530b00be2edd5714f2a2540d70
MohithTP <tpmohith13@gmail.com>
2025-04-09 19:16:06 +0530

    quicksort update

--- QuickSort.c
+++ QuickSort.c
@@ -31,7 +31,16 @@ void quicksort(int arr[],int s,int e){
     }
 }
 
+int duplicates(int arr[],int n){
+    for(int i=0;i<n;i++){
+        if(arr[i]=arr[i+1]){
+            return arr[i+1];
+            return i;
+        }
+    }
+    return -1;
 
+}
 
 int main(){
     int n;
